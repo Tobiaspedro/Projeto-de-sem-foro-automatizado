c28a99cdc93c38de6e63a4bbd4f91ef4df6dcc78
Tobiaspedro <51788147+Tobiaspedro@users.noreply.github.com>
2019-08-07 18:55:24 -0300

    Add files via upload

---
+++ semaforo.ino
@@ -0,0 +1,26 @@
+//Projeto de sinal de transito no Arduiono
+//por Tobias Pedro Eustáquio T4
+
+
+void setup (){
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+}
+
+void loop(){
+  //controle do led verde
+  digitalWrite(8,HIGH);  //acende o led
+  delay(5000);           // espera 5 segundos
+  digitalWrite(8,LOW);   //apaga o led
+  
+  //controle do led amarelo
+  digitalWrite(9,HIGH);  //acende o led
+  delay(2000);           //espera 2 segundos
+  digitalWrite(9,LOW);   // apaga o led
+  
+  //controle do led vermelho
+  digitalWrite(10,HIGH); //acende o led
+  delay(5000);           //espera 5 segundos
+  digitalWrite(10,LOW);  //apaga o led
+}
